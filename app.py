a07979d42a72daa9f31ab0b1d310748784b27d71
MontyWinget <monty.winget@gmail.com>
2022-02-26 11:51:56 +0530

    Cross-origin get and post request

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify, json
+from flask_cors import CORS, cross_origin
 from flask_mysqldb import MySQL
 import yaml
 
 # Object init to run flask app
 app = Flask(__name__)
+CORS(app)
 
 # Configure DB
 db = yaml.full_load((open('db.yaml')))
@@ -15,31 +17,52 @@ app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
+@cross_origin()
 def index():
-    if request.method == 'POST':
-        # Fetch form data
-        user_details = request.form
-        name = user_details['Name']
-        email = user_details['Email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email) Values(%s, %s)", (name, email))
-        mysql.connection.commit()
-        cur.close()
-        return redirect('/users')
-    return render_template('index.html')
+    data = request.get_json()
+    name = data['name']
+    email = data['email']
+    print(name)
+    print(email)
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO users(name, email) Values(%s, %s)", (name, email))
+    mysql.connection.commit()
+    cur.close()
+    # return redirect('/users')
+    return "Success"
 
 
 @app.route('/users')
+@cross_origin()
 def users():
     cursor = mysql.connection.cursor()
     cur = cursor
     result_value = cur.execute("SELECT * FROM users")
     if result_value > 0:
-        user_details = cur.fetchall()
-        return render_template('users.html', user_details=user_details)
+        row_headers = [x[0] for x in cur.description]  # this will extract row headers
+        rv = cur.fetchall()
+        json_data = []
+        for result in rv:
+            json_data.append(dict(zip(row_headers, result)))
+        return jsonify(json_data)
+    return jsonify(welcome='cross-origin working')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+#
+#     if result_value > 0:
+#         # user_details = cur.fetchall()
+#         row_headers = [x[0] for x in cur.description]  # this will extract row headers
+#         rv = cur.fetchall()
+#         json_data = []
+#         for result in rv:
+#             json_data.append(dict(zip(row_headers, result)))
+#         return jsonify(json_data)
+#         # for user in user_details:
+#         #     user_details_dict['Name'] = user[0]
+#         #     user_details_dict['Email'] = user[1]
+#         # # return render_template('html.file', user_details=user_details)
+#         # return user_details_dict
+#     return jsonify(welcome='cross-origin working')
